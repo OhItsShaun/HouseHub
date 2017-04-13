310a241d691f2e6d6af86b371dfe0c5deb90c6d3
Shaun Merchant <shaun.a.merchant@icloud.com>
2017-04-13 01:20:11 +0100

    HouseRuntime.run wrapper

--- Sources/HouseHub/HouseNetworkExtension.swift
+++ Sources/HouseHub/HouseNetworkExtension.swift
@@ -11,6 +11,7 @@ import HouseCore
 
 public extension HouseNetwork {
     
+    /// Open the House Network and set up any necessary packages.
     public func open() {
         HouseHubServices.registerAll()
         HouseNetwork.current().start()
@@ -18,3 +19,15 @@ public extension HouseNetwork {
     }
     
 }
+
+public extension HouseRuntime {
+    
+    /// Run the process as a House Hub.
+    ///
+    /// - Parameter process: The House Process to run.
+    public static func run(process: HouseProcess) {
+        HouseRuntime.run(process, as: .houseHub)
+    }
+    
+}
+
